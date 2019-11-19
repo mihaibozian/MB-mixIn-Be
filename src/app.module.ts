ab4bc122685c4775376fd25e406a74dfeb88b8dd
Corina Titica <corina.alexandra.titica@ibm.com>
2019-11-19 12:34:25 +0200

    set config for monguse

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MongooseModule} from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://admin:170096@cluster0-ulutz.mongodb.net/mixin?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
