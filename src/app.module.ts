07bbc2ff3b1334c1adde5a425f6c5b5be3b845e6
mihaibozian <mihai.bozian@gmail.com>
2019-11-30 19:32:41 +0200

    be update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {MongooseModule} from '@nestjs/mongoose';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://admin:170096@cluster0-ulutz.mongodb.net/mixin?retryWrites=true&w=majority')],
+  imports: [MongooseModule.forRoot('mongodb+srv://mihai:ziua1392@mb-f6slw.mongodb.net/test?retryWrites=true&w=majority'), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
